cbd41145c9eeb9620a806478422209c7257b7bf3
Alex <alexarasanz@gmail.com>
2023-06-11 16:30:51 +0200

    added unitary test for the getMaximumPriorityPrice functionality

--- app/src/test/java/com/bcncgroup/test/app/AppApplicationTests.java
+++ app/src/test/java/com/bcncgroup/test/app/AppApplicationTests.java
@@ -1,13 +1,11 @@
 package com.bcncgroup.test.app;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void main() {
+		AppApplication.main(new String[] {});
 	}
-
 }
